5bda5bc4a650125c670cfd8726ca0e6cc9f448ed
Jonnyfencing1 <conneryjonathan@gmail.com>
2021-03-08 20:59:18 +0000

    Initial Upload

---
+++ discord.lua
@@ -0,0 +1,19 @@
+
+Citizen.CreateThread(function()
+    while true do
+        local player = GetPlayerPed(-1)
+        
+        Citizen.Wait(5*1000) -- checks every 5 seconds (to limit resource usage)
+        
+        SetDiscordAppId() -- client id (int)
+
+        SetRichPresence( GetPlayerName(source) .. " is on " .. GetStreetNameFromHashKey(GetStreetNameAtCoord(table.unpack(GetEntityCoords(player))) )) -- main text (string)
+
+        SetDiscordRichPresenceAsset("big") -- large logo key (string)
+        SetDiscordRichPresenceAssetText(GetPlayerName(source)) -- Large logo "hover" text (string)
+
+        SetDiscordRichPresenceAssetSmall("zua") -- small logo key (string)
+        SetDiscordRichPresenceAssetSmallText("Health: "..(GetEntityHealth(player)-100)) -- small logo "hover" text (string)
+
+    end
+end)
